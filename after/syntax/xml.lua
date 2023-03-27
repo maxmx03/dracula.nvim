48bdf295be74d2c4f9a924b59e72af6b1162ba46
Max Miliano <milianordelcanto@gmail.com>
2023-03-26 22:42:59 -0300

    fix typo

--- after/syntax/xml.lua
+++ after/syntax/xml.lua
@@ -1,4 +1,4 @@
 vim.api.nvim_set_hl(0, 'xmlTagName', { link = '@tag' })
 vim.api.nvim_set_hl(0, 'xmlTag', { link = '@tag.delimiter' })
 vim.api.nvim_set_hl(0, 'xmlAttrib', { link = '@tag.attribute' })
-vim.api.nvim_set_hl(0, 'xmlProcessingDelim', { link = '@tab.delimiter' })
+vim.api.nvim_set_hl(0, 'xmlProcessingDelim', { link = '@tag.delimiter' })
