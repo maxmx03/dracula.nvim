0d5b0752f5b4292eadd410e8ab129299af041c7f
Max Miliano <milianordelcanto@gmail.com>
2023-03-28 17:49:53 -0300

    update: css queries

---
+++ after/queries/css/highlights.scm
@@ -0,0 +1,12 @@
+; extends
+((class_name) @function (#set! "priority" 125))
+((plain_value) @constant (#set! "priority" 125))
+(tag_name) @tag
+(id_name) @function
+(unit) @keyword
+(property_name) @type
+
+"#" @keyword
+"," @keyword
+":" @keyword
+"." @keyword
