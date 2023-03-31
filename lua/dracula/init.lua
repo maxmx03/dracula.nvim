802ec743569a6c51ab5afab7c9fac7cd0d419a3e
Max Miliano <milianordelcanto@gmail.com>
2023-03-31 14:10:50 -0300

    fix bug

--- lua/dracula/init.lua
+++ lua/dracula/init.lua
@@ -4,13 +4,15 @@ local Config = require("dracula.config")
 local M = Config:new()
 
 function M:apply_transparency()
-	local hl = vim.api.nvim_set_hl
-	local c = self.colors
+	if self.transparent then
+		local hl = vim.api.nvim_set_hl
+		local c = self.colors
 
-	hl(0, "Normal", { fg = c.fg, bg = "NONE" })
-	hl(0, "SignColumn", { fg = "NONE", bg = "NONE" })
-	hl(0, "CursorLineNr", { fg = c.pink, bg = "NONE" })
-	hl(0, "LineNr", { fg = c.comment, bg = "NONE" })
+		hl(0, "Normal", { fg = c.fg, bg = "NONE" })
+		hl(0, "SignColumn", { fg = "NONE", bg = "NONE" })
+		hl(0, "CursorLineNr", { fg = c.pink, bg = "NONE" })
+		hl(0, "LineNr", { fg = c.comment, bg = "NONE" })
+	end
 end
 
 function M.load_default()
@@ -37,13 +39,13 @@ M.setup = function(user_config)
 
 	M:set_soft(M.user_config.soft)
 	M:set_transparent(M.user_config.transparent)
-  M:set_callback(M.user_config.callback)
+	M:set_callback(M.user_config.callback)
 	M:set_colors()
 	M:set_user_colors()
 
 	theme.set_highlights(M.colors)
-  M:apply_transparency()
-  M.callback(M.colors)
+	M:apply_transparency()
+	M.callback(M.colors)
 end
 
 return M
