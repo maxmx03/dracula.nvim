d5e40fff905cd92d85bcb1fd2be003753f1f0d28
Max Miliano <milianordelcanto@gmail.com>
2023-04-21 03:08:56 -0300

    fix lualine

--- lua/lualine/themes/dracula.lua
+++ lua/lualine/themes/dracula.lua
@@ -1,47 +1,35 @@
 local dracula = require("dracula")
-local c = dracula.colors
-
-local colors = {
-	bgdark = c.float_bg,
-	lightgray = c.comment,
-	blended_purple = c.blended_purple,
-	purple = c.purple,
-	red = c.red,
-	yellow = c.yellow,
-	green = c.green,
-	white = c.fg,
-	bg = c.bg,
-}
+local colors = dracula.colors
 
 return {
 	normal = {
 		a = { bg = colors.purple, fg = colors.bg, gui = "bold" },
-		b = { bg = colors.lightgray, fg = colors.white },
-		c = { bg = colors.bgdark, fg = colors.white },
+		b = { bg = colors.comment, fg = colors.fg },
+		c = { bg = colors.bgdark, fg = colors.fg },
 	},
 	insert = {
 		a = { bg = colors.green, fg = colors.bg, gui = "bold" },
-		b = { bg = colors.lightgray, fg = colors.white },
-		c = { bg = colors.bgdark, fg = colors.white },
+		b = { bg = colors.comment, fg = colors.fg },
+		c = { bg = colors.bgdark, fg = colors.fg },
 	},
 	visual = {
 		a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
-		b = { bg = colors.lightgray, fg = colors.white },
-		c = { bg = colors.bgdark, fg = colors.white },
+		b = { bg = colors.comment, fg = colors.fg },
+		c = { bg = colors.bgdark, fg = colors.fg },
 	},
 	replace = {
 		a = { bg = colors.red, fg = colors.bg, gui = "bold" },
-		b = { bg = colors.lightgray, fg = colors.white },
-		c = { bg = colors.bgdark, fg = colors.white },
+		b = { bg = colors.comment, fg = colors.fg },
+		c = { bg = colors.bgdark, fg = colors.fg },
 	},
 	command = {
 		a = { bg = colors.blended_purple, fg = colors.purple, gui = "bold" },
-		b = { bg = colors.lightgray, fg = colors.white },
-		c = { bg = colors.bgdark, fg = colors.white },
+		b = { bg = colors.comment, fg = colors.fg },
+		c = { bg = colors.bgdark, fg = colors.fg },
 	},
 	inactive = {
-		a = { bg = colors.bgdark, fg = colors.white, gui = "bold" },
-		b = { bg = colors.lightgray, fg = colors.white },
-		c = { bg = colors.bgdark, fg = colors.white },
+		a = { bg = colors.bgdark, fg = colors.fg, gui = "bold" },
+		b = { bg = colors.comment, fg = colors.fg },
+		c = { bg = colors.bgdark, fg = colors.fg },
 	},
 }
