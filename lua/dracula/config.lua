1db679774f5bdbe8902e235f8538926f47034318
max miliano <milianordelcanto@gmail.com>
2024-02-26 09:35:48 -0300

    fix on_highlights type annotation

--- lua/dracula/config.lua
+++ lua/dracula/config.lua
@@ -1,6 +1,6 @@
 ---@class dracula.config
 ---@field transparent? boolean
----@field on_highlights? fun(colors: dracula.palette, color: table): dracula.highlights
+---@field on_highlights? fun(colors: dracula.palette, color: dracula.color): dracula.highlights
 ---@field on_colors? fun(colors: dracula.palette, color: dracula.color): dracula.palette
 return {
   transparent = false,
