f6552f53428b58c082eada492d4de6efba8d1f18
max miliano <milianordelcanto@gmail.com>
2024-03-02 10:18:49 -0300

    fix: css psudo_class_selector

--- after/queries/css/highlights.scm
+++ after/queries/css/highlights.scm
@@ -1,5 +1,5 @@
 ; extends
-((pseudo_class_selector) @type (#set! "priority" 125))
+((pseudo_class_selector) ((class_name) @type (#set! "priority" 125)))
 ((class_selector) @variable.parameter (#set! "priority" 125))
 ((plain_value) @constant (#set! "priority" 125))
 ((id_selector) @variable.parameter (#set! "priority" 125))
