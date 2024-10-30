b7d73e212c0d384808f78e2bd2477f854d338171
Max Miliano <milianordelcanto@gmail.com>
2024-10-30 11:15:52 -0300

    fix: css highlights

---
+++ after/queries/css/highlights.scm
@@ -0,0 +1,8 @@
+; extends
+((plain_value) @constant (#set! "priority" 125))
+(unit) @keyword
+(keyframes_name) @variable.parameter
+(rule_set
+  (selectors
+    (id_selector
+      (id_name) @function)))
